8fbc904c4a0b3ea3caafe1dc0a168b3d97a9955c
qink0043 <3208013769@qq.com>
2025-05-20 17:04:17 +0800

    修改已知问题

--- routes/bgmProxy.js
+++ routes/bgmProxy.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const axios = require('axios')
-const rateLimit = require('exporess-rate-Limit')
+const rateLimit = require('express-rate-Limit')
 
 const router = express.Router()
 
