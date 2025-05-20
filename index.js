c49571b0e1063394af8557e9fbb4d5766d3fac5e
qink0043 <3208013769@qq.com>
2025-05-20 16:24:20 +0800

    url修改

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
 
-const authRoutes = require('./routes/auth');
+const authRoutes = require('./routes/auth')
 const bgmProxyRoutes = require('./routes/bgmProxy')
 
-const app = express();
+const app = express()
 app.use(cors({
   origin: '*', // 前端地址
   credentials: true
-}));
-app.use(express.json());
+}))
+app.use(express.json())
 
-app.use('/api', authRoutes);
-app.use('api/bgm', bgmProxyRoutes)
+app.use('/api/user', authRoutes)
+app.use('/api/bgm', bgmProxyRoutes)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`)
 });
