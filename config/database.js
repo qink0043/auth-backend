64f124a8a2a675786f4838d00008c7125ce3e005
qink0043 <3208013769@qq.com>
2025-06-15 17:58:34 +0800

    用户名密码

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  user: "root", // 数据库的用户名
+  user: "vue_auth", // 数据库的用户名
   password: "123456", // 数据库的密码
   host: "43.136.72.202", // 数据服务地址
   port: "3306", // 数据库端口号
