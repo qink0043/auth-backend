f772fc758569e528d76bdaf608cfabcdacd63341
qink0043 <3208013769@qq.com>
2025-06-15 17:20:21 +0800

    连接服务器数据库

--- config/database.js
+++ config/database.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   user: "root", // 数据库的用户名
   password: "2004qcq310", // 数据库的密码
-  host: "localhost", // 数据服务地址
+  host: "http://43.136.72.202/", // 数据服务地址
   port: "3306", // 数据库端口号
-  database: "vue_auth_login", // 数据库名称
+  database: "vue_auth", // 数据库名称
   dateStrings: true, // 规定查询时间字段的显示形式
 });
 
